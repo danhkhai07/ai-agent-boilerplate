320e04ce56c1117d6003c53bc2980fd848877892
danhkhai07 <aliveomg@gmail.com>
2026-07-10 18:38:07 +0700

    refactor: move tools to a seperate folder

--- internal/mcp/tool.go
+++ internal/mcp/tool.go
@@ -1,19 +1,10 @@
 package mcp
 
 import (
+	"agent/internal/mcp/tools"
 	mcp_sdk "github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
-type Tool struct {
-	SDKTool mcp_sdk.Tool
-	Handler mcp_sdk.ToolHandlerFor[any, any]
-}
-
 func bindTools(server *mcp_sdk.Server) {
-	timeTool := mcp_sdk.Tool{
-		Name: "checkTime",
-		Description: "get the time at the current moment in hanoi, nyc, beijing",
-	}
-
-	mcp_sdk.AddTool(server, &timeTool, checkTime)
+	mcp_sdk.AddTool(server, &tools.CheckTimeTool, tools.CheckTimeHandler)
 }
