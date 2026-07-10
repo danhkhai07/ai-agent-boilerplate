9fa9b7310df5834ef73f53b3ecac84aa004338e1
danhkhai07 <aliveomg@gmail.com>
2026-07-10 23:45:50 +0700

    feat: base agent algorithm

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -2,6 +2,18 @@ package agent
 
 import (
 	"agent/internal/mcp"
+	"context"
+
+	mcp_sdk "github.com/modelcontextprotocol/go-sdk/mcp"
+)
+
+type Role string
+
+const (
+	UserRole Role = "User"
+	SystemRole Role = "System"
+	AgentRole Role = "Agent"
+	ToolRole Role = "Tool"
 )
 
 type Agent struct {
@@ -10,16 +22,61 @@ type Agent struct {
 }
 
 type Message struct {
-	Role string
+	Role Role
 	Content string
 }
 
-func (a *Agent) Call(input string) (error) {
-	// messages := []Message{
-	// 	{Role: "User", Content: input },
-	// }
+type Context struct {
+	Messages []Message
+	Tools []mcp_sdk.Tool
+}
+
+func (a *Agent) Call(ctx context.Context, input string) (string, error) {
+	tools, err := a.MCPClient.Tools(ctx)
+	if err != nil {
+		return "", err
+	}
+	agentContext := Context{
+		Tools: tools,
+	}
+	// System prompt
+	agentContext.Messages = append(agentContext.Messages, Message{
+		Role: SystemRole,	
+		Content: INITIAL_SYSTEM_PROMPT,
+	})
+	// User initial input
+	agentContext.Messages = append(agentContext.Messages, Message{
+		Role: UserRole,
+		Content: input,
+	})
 
 	for {
-				
+		chatOutput, err := a.LLM.Chat(ctx, agentContext)
+		if err != nil {
+			return "", err
+		}
+
+		if a.LLM.IsToolCall(chatOutput) {
+			toolName, params, err := a.LLM.ToToolCall(chatOutput)
+			if err != nil {
+				return "", err
+			}
+
+			toolOutput, err := a.MCPClient.CallTool(ctx, toolName, params)
+			if err != nil {
+				return "", err
+			}
+			toolMessage := Message{
+				Role: ToolRole,
+				Content: toolOutput,
+			}
+			agentContext.Messages = append(agentContext.Messages, toolMessage)
+			continue
+		}
+
+		if a.LLM.IsText(chatOutput) {
+			return chatOutput, nil
+		}
 	}
 }
+
--- internal/agent/llm.go
+++ internal/agent/llm.go
@@ -1,5 +1,11 @@
 package agent
 
+import "context"
+
 type LLMClient interface {
-	 Chat(input string) (string, error)
+	 Chat(ctx context.Context, agentContext Context) (string, error)
+	 IsText(s string) bool
+	 IsToolCall(s string) bool
+	 ToText(s string) (string, error)
+	 ToToolCall(s string) (string, map[string]any, error)
 }
